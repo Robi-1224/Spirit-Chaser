3fd842636bde1d84d151226b9d8d08158a520d24
Robi-1224 <230087@student.glu.nl>
2025-03-05 10:30:41 +0100

    movement working with new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    PlayerInput playerInput;
+    InputAction MovementInput;
+    [SerializeField] int speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        MovementInput = playerInput.actions.FindAction("Movement");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    public void Movement()
+    {
+        Vector2 dir = MovementInput.ReadValue<Vector2>();
+        transform.position += new Vector3(dir.x, 0,dir.y) * speed * Time.deltaTime;
     }
 }
