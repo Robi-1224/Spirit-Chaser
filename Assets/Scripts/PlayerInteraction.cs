a6859b30ab8d5f6704eb9786b102ff0da0f6ff05
Robi-1224 <145546915+Robi-1224@users.noreply.github.com>
2025-03-11 16:33:50 +0100

    raycast added to detect objects

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,37 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : RitualObject
 {
     [SerializeField] GameObject heldObject;
 
-    public void Interaction(GameObject other)
-    {
-      
 
-       if (other.CompareTag("Held item"))
-       {
-            heldObject = other.gameObject;
-       }
-       else if(other.CompareTag("No held item ritual"))
-       {
+    PlayerInput playerInput;
+    InputAction interaction;
+
+    RaycastHit hit;
+
+    private void Awake()
+    {
+        playerInput= GetComponent<PlayerInput>();
+        interaction = playerInput.actions.FindAction("Interaction");
+    }
+
+    public void interact()
+    {
+        Physics.Raycast(transform.position,Vector3.forward,out hit,2.5f);
+        
+        if (hit.collider.gameObject.CompareTag("Held item"))
+        {
+            heldObject = hit.collider.gameObject;
+        }
+        else if (hit.collider.gameObject.CompareTag("No held item ritual"))
+        {
             ItemBehaviour();
-       }
-       else if(other.CompareTag("Held item ritual") && heldObject!= null)
-       {
+        }
+        else if (hit.collider.gameObject.CompareTag("Held item ritual") && heldObject != null)
+        {
             ItemBehaviour();
-       }
-       else
-       {
+        }
+        else
+        {
             Debug.Log("Need the held item");
-       }
-
-      
+        }
     }
+    
 
-    private void OnTriggerEnter(Collider other)
-    {
-       Interaction(other.gameObject);
-    }
 }
