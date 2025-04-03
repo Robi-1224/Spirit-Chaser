ecf2696f53758885ac918e0309574d2638dc4db1
Robi-1224 <145546915+Robi-1224@users.noreply.github.com>
2025-04-03 22:03:47 +0200

    arrow keys added and tutorial text updated

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.EditorCoroutines.Editor;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class PlayerInteraction : MonoBehaviour
 {
